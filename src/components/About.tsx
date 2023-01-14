54f2c9befb7140db206dd094b37c244cf926f969
gigiul <luigidalleaste@gmail.com>
2023-01-14 12:07:20 +0100

    fixed div streching while TypeAnimation generate the text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
+        <div className='max-w-[95%] m-auto mt-4'>
             <p>About</p>
         </div>
     )
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,20 +6,49 @@ import { TypeAnimation } from 'react-type-animation';
 const Home = () => {
   return (
     <div 
-        className='max-w-[95%] m-auto mt-4 pb-32' 
+        className='max-w-[95%] h-[500px] m-auto mt-4 p-16 flex flex-col items-center justify-center' 
         style={{ backgroundImage: `url(${background})`}}
     >
-    <div className='flex justify-center pt-40'>
-        <div className='flex flex-col justify-center items-center bg-black/80 w-[500px] py-4 rounded-3xl'>
+        
+        <div className='flex flex-col items-center justify-center bg-black/80 w-[500px] h-[500px] rounded-3xl p-4'>
             <img    
                 src={profile}
                 alt="profile"
                 className='rounded-full w-44 h-44'
             />
-        <div className='flex flex-col justify-center text-center pt-4 gap-4'>
             <h1 className='text-bold'>Luigi <span className='inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-600'>Dalle Aste</span></h1>
             <TypeAnimation
-        className='text-sm italic max-w-[50%] m-auto text-[#A0A1A0]'
+                className='text-sm italic max-w-[55%] text-[#A0A1A0]'
+                sequence={[
+                '"My curiosity and my desire to learn new things allows me to approach problems and situations from different perspectives"', // Types 'One'
+                3000, // Waits 1s
+                ''
+                ]}
+                wrapper="div"
+                cursor={true}
+                repeat={Infinity}
+                speed={60}
+                deletionSpeed={80}
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Home
+
+
+        {/*<div className='flex flex-col items-center justify-center bg-black/80 w-[500px] rounded-3xl'>
+            <img    
+                src={profile}
+                alt="profile"
+                className='rounded-full w-44 h-44'
+            />
+            <h1 className='text-bold'>Luigi <span className='inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-600'>Dalle Aste</span></h1>
+        <div className='flex flex-col justify-center text-center pt-4 gap-4'>
+            
+             <TypeAnimation
+        className='text-sm italic max-w-[55%] m-auto text-[#A0A1A0]'
       sequence={[
         '"My curiosity and my desire to learn new things allows me to approach problems and situations from different perspectives"', // Types 'One'
         3000, // Waits 1s
@@ -30,13 +59,6 @@ const Home = () => {
       repeat={Infinity}
       speed={60}
       deletionSpeed={80}
-    />
+    /> */}
 {/*             <p className='text-sm italic max-w-[50%] m-auto text-[#A0A1A0]'>"My curiosity and my desire to learn new things allows me to approach problems and situations from different perspectives"</p>
- */}        </div>
-        </div>
-    </div>
-    </div>
-  )
-}
-
-export default Home
+ */}
